3805f3627149e06bbcf32722f606fd1ea4d8cda2
koki0430 <89891689+koki0430@users.noreply.github.com>
2022-02-07 12:48:59 +0900

    made better

--- second total.py
+++ second total.py
@@ -8,34 +8,76 @@ from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 from pybricks.iodevices import I2CDevice
 import time
-
+# ----------------------------------------------------------------------------------------------------
 # センサのインスタンスの生成
-CS1 = ColorSensor(PortS1)
-CS2 = ColorSensor(PortS2)
-CS3 = ColorSensor(PortS3)
-CS4 = ColorSensor(PortS4)
+ColorSensor1 = ColorSensor(PortS1)
+ColorSensor2 = ColorSensor(PortS2)
+ColorSensor3 = ColorSensor(PortS3)
+ColorSensor4 = ColorSensor(PortS4)
 
 TS_left = TouchSensor(portS)
 TS_right = TouchSensor(portS)
 
-sensor = Sensor()
-
+# ----------------------------------------------------------------------------------------------------
 # モーターのインスタンスの生成
 left_motor = Motor(port)
 right_motor = Motor(port)
 SYAKKE = DriveBase(left_motor, right_motor, wheel_diameter=tmp, axle_track=tmp)
-
+# ----------------------------------------------------------------------------------------------------
 # 変数の定義
 value_sensor = []
-
-#PID制御
 black_hightest_refrection = 10
 silber_lowest_refrection = 40
+refrection_last = (0,0)
+
+#PID制御
 Kp = 1.5
 Ki = 0.5
 Kd = 1.3
-individual_difference = 0 #cs2-cs3　個体差
+individual_difference = 0 #ColorSensor2-ColorSensor3　個体差
 errors = [0,0,0,0,0]
+# ----------------------------------------------------------------------------------------------------
+class Sensor:
+    def color_aggregation(self,list):
+        value_sensor.clear()
+        if 1 in list:
+            value_sensor.append(ColorSensor1.color())
+        if 2 in list:
+            value_sensor.append(ColorSensor2.color())
+        if 3 in list:
+            value_sensor.append(ColorSensor3.color())
+        if 4 in list:
+            value_sensor.append(ColorSensor4.color())
+        return tuple(value_sensor)
+    
+    def color_all(self):
+        return ColorSensor1.color(), ColorSensor2.color(), ColorSensor3.color(), ColorSensor4.color()
+    
+    def refrection_aggregation(self,list):
+        value_sensor.clear()
+        if 1 in list:
+            value_sensor.append(ColorSensor1.refrection())
+        if 2 in list:
+            value_sensor.append(ColorSensor2.refrection())
+        if 3 in list:
+            value_sensor.append(ColorSensor3.refrection())
+        if 4 in list:
+            value_sensor.append(ColorSensor4.refrection())
+        return tuple(value_sensor)
+    
+    def refrection_all(self):
+        return ColorSensor1.refrection(), ColorSensor2.refrection(), ColorSensor3.refrection(), ColorSensor4.refrection()
+    
+    def touch(self):
+        return TS_left.pressed(), TS_right.pressed()
+
+sensor = Sensor()
+# ----------------------------------------------------------------------------------------------------
+def speedL(self, rotational_speed):
+    return rotational_speed * 1050
+        
+def speedM(self, rotational_speed):
+    return rotational_speed * 1560
 
 class Motor:
     def pid_control(self, base_power = 30):
@@ -47,59 +89,46 @@ class Motor:
 
         SYAKKE.drive(speedL(base_power), u)
 
-    class Tank:
-        def speed(self, base_power, steering):
-            SYAKKE.drive(speedL(base_power), steering)
+    def speed(self, base_power, steering):
+        SYAKKE.drive(speedL(base_power), steering)
         
-        def angle(self, base_power, steering, target_angle, stop_type = Stop.BREAK):
-            left_motor.reset_angle(0)
-            right_motor.reset_angle(0)
+    def angle(self, base_power, steering, target_angle, stop_type = Stop.BREAK):
+        left_motor.reset_angle(0)
+        right_motor.reset_angle(0)
 
-            SYAKKE.drive(speedL(base_power), steering)
-            while abs(left_motor.angle()) < target_angle or abs(right_motor.angle()) < target_angle:
-                wait(10)
-            SYAKKE.stop(stop_type)
+        SYAKKE.drive(speedL(base_power), steering)
+        while abs(left_motor.angle()) < target_angle or abs(right_motor.angle()) < target_angle:
+            wait(10)
+        SYAKKE.stop(stop_type)
         
-        def time(self, base_power, steering, run_time, stop_type = Stop.BREAK):
-            SYAKKE.drive_time(speedL(base_power), steering, run_time)
-            SYAKKE.stop(stop_type)
+    def time(self, base_power, steering, run_time, stop_type = Stop.BREAK):
+        SYAKKE.drive_time(speedL(base_power), steering, run_time)
+        SYAKKE.stop(stop_type)
 
-def speedL(self, rotational_speed):
-    return rotational_speed * 1050
-        
-def speedM(self, rotational_speed):
-    return rotational_speed * 1560
+motor = Motor()
+tank = motor.Tank()
+# ----------------------------------------------------------------------------------------------------
+class Turn:
+    pass
 
-class Sensor:
-    def color_part(self,list):
-        value_sensor.clear()
-        if 1 in list:
-            value_sensor.append(CS1.color())
-        if 2 in list:
-            value_sensor.append(CS2.color())
-        if 3 in list:
-            value_sensor.append(CS3.color())
-        if 4 in list:
-            value_sensor.append(CS4.color())
-        return tuple(value_sensor)
+    def rescue(self):
+        pass
+
+turn = Turn()
+# ----------------------------------------------------------------------------------------------------
+
+#ボタン入力があるまで待機
+while not any(ev3.bottons.pressed()):
+    wait(10)
+
+while True:
+    #silber
+    if sum([i > silber_lowest_refrection for i in sensor.refrection_aggregation()]) >= 2:
+        turn.rescue()
     
-    def color_all(self):
-        return CS1.color(), CS2.color(), CS3.color(), CS4.color()
-    
-    def refrection_part(self,list):
-        value_sensor.clear()
-        if 1 in list:
-            value_sensor.append(CS1.refrection())
-        if 2 in list:
-            value_sensor.append(CS2.refrection())
-        if 3 in list:
-            value_sensor.append(CS3.refrection())
-        if 4 in list:
-            value_sensor.append(CS4.refrection())
-        return tuple(value_sensor)
-    
-    def refrection_all(self):
-        return CS1.refrection(), CS2.refrection(), CS3.refrection(), CS4.refrection()
-    
-    def touch(self):
-        return TS_left.pressed(), TS_right.pressed()
+    # black
+    refrection_now = sensor.refrection_aggregation((1,4))
+    if 10 < refrection_last[0] - refrection_now[0] or CS1.color() == 1:
+        turn.turn_black("left")
+    if 10 < refrection_last[1] - refrection_now[1] or CS4.color() == 1:
+        turn.turn_black("right")
