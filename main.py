5ec838f1c7c2d8b547cfd602f38b9dd7b7f40b12
koki0430 <89891689+koki0430@users.noreply.github.com>
2022-02-12 21:41:15 +0900

    made better

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-# 読み込んでいく！
+
+from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MoveSteering
+from ev3dev2.sensor import INPUT_1, INPUT_2, INPUT_3, INPUT_4, Sensor
+from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor
+from ev3dev2.port import LegoPort
+
+# from ev3dev.brickpi import *
+from ev3dev.ev3 import *
+from ev3dev.auto import *
+
 from time import sleep, time
 import sys
 
-from ev3dev2.motor import LargeMotor, MediumMotor, OUTPUT_A, OUTPUT_B, OUTPUT_C, OUTPUT_D, SpeedPercent, MoveTank, MoveSteering
-from ev3dev2.sensor import INPUT_1, INPUT_B, INPUT_C, INPUT_D
-from ev3dev2.sensor.lego import TouchSensor, ColorSensor, UltrasonicSensor
-
 from ev3dev2.button import Button
 from ev3dev2.sound import Sound
+sleep_time = 1
 
+# Mux-wise--------------------------------------------------------------------------------------------
 # mux sensor port
 muxC1port = LegoPort("in4:i2c80:mux1")  # TouchSensor
 muxC2port = LegoPort("in4:i2c81:mux2")  # TouchSensor
@@ -18,7 +25,7 @@ muxC3port = LegoPort("in4:i2c82:mux3")    # (driver_name="ms-ev3-smux")
 # setting the 1st port on SensorMUX to analogue mode, to be used for touchsensor
 muxC2port.mode = "analog"
 muxC3port.mode = "analog"
-sleep(1) # need to wait for analog mode to be set
+sleep(sleep_time) # need to wait for analog mode to be set
 
 # loading the devices for each port
 muxC1port.set_device = "lego-ev3-color"
@@ -28,7 +35,20 @@ muxC3port.set_device = "lego-ev3-touch"
 colorsensor4 = ColorSensor("in4:i2c80:mux1")
 touch_sensor_left = TouchSensor("in4:i2c81:mux2")
 touch_sensor_right = TouchSensor("in4:i2c82:mux3")
-sleep(1) # need to wait for sensors to be loaded. 0.5 seconds is not enough.
+sleep(sleep_time) # need to wait for sensors to be loaded. 0.5 seconds is not enough.
+
+colorsensor4.mode = "COL-REFLECT"
+# ----------------------------------------------------------------------------------------------------
+
+colorsensor1 = ColorSensor(INPUT_1)
+colorsensor2 = ColorSensor(INPUT_2)
+colorsensor3 = ColorSensor(INPUT_3)
+
+motor_left = LargeMotor(OUTPUT_A)
+motor_right = LargeMotor(OUTPUT_B)
+
+botton = Button() # bottons of the brick
+sound = Sound()
 
 class Sensors_color:
     def __init__(self,port):
@@ -36,8 +56,12 @@ class Sensors_color:
     
     def color(self):
         if self.port == 4:
+            sound.speak("Load color sensor")
             colorsensor4.mode = "COL-COLOR"
-            pass
+            sleep(sleep_time)
+            return colorsensor4.value()
+        elif self.port == 3:
+
     
     def refrect(self):
         pass
@@ -48,8 +72,5 @@ class Sensors_touch:
     
     def pressed(self):
         return touch_sensor_left.value() if self.port == 5 else touch_sensor_right.value()
-
-botton = Button() # bottons of the brick
-sound = Sound()
 
 botton.wait_for_pressed(['enter']) # wait for "enter" botton to be pressed
