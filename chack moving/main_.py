ef25fba2692d7f1e27338384f6fa307683768e42
koki0430 <89891689+koki0430@users.noreply.github.com>
2022-04-28 09:14:36 +0900

    add hsv

--- chack moving/main_.py
+++ chack moving/main_.py
@@ -10,17 +10,29 @@ watch = StopWatch()
 col = ColorSensor(Port.S1)
 
 while True:
-    rgb = col.rgb()
+    try:
+        rgb = col.rgb()
 
-    if rgb[1] < rgb[0] > rgb[2]: # Red
-        hue = 60 * ((rgb[1] - rgb[2]) / (max(rgb) - min(rgb)))
-    elif rgb[0] < rgb[1] > rgb[2]: # Green
-        hue = 60 * ((rgb[2] - rgb[0]) / (max(rgb) - min(rgb))) + 120
-    else: # Blue
-        hue = 60 * ((rgb[0] - rgb[1]) / (max(rgb) - min(rgb))) + 240
+        # Hue
+        if rgb[1] < rgb[0] > rgb[2]: # Red
+            hue = 60 * ((rgb[1] - rgb[2]) / (max(rgb) - min(rgb)))
+        elif rgb[0] < rgb[1] > rgb[2]: # Green
+            hue = 60 * ((rgb[2] - rgb[0]) / (max(rgb) - min(rgb))) + 120
+        else: # Blue
+            hue = 60 * ((rgb[0] - rgb[1]) / (max(rgb) - min(rgb))) + 240
 
-    if hue < 0:
-        hue += 360
+        if hue < 0:
+            hue += 360
 
-    ev3.screen.clear()
-    ev3.screen.print(str(rgb) +"_" + str(hue))
+        # Saturation
+        saturation = (max(rgb) - min(rgb)) / max(rgb)
+
+        # Brightness
+        brightness = max(rgb)
+
+        ev3.screen.clear()
+        ev3.screen.print(str(rgb) +"_" + str(hue) +"_" + str(saturation) +"_" + str(brightness))
+
+    except TypeError as error:
+        print(error)
+        print(type(error))
