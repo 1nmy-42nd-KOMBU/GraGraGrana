b15088456c56b8da22df93292a25a36becc8d278
koki0517 <koki1743ok@gmail.com>
2023-02-09 01:31:54 +0900

    緑マーカーの左折と右折を追加

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ colorRight = ColorSensor(Port.S4)
 
 motorLeft = Motor(Port.A)
 motorRight = Motor(Port.D)
+
+highest_refrection_of_Black = 15
 # ============================================================
 
 class Tank:
@@ -151,14 +153,12 @@ def onGreenMarker(direction):
         start_angle_deg = motorRight.angle()
         isRightGreen = False
 
-        runleft = motorLeft.run(powertodegs(basic_speed)) # caching object references
-        runRight = motorRight.run(powertodegs(basic_speed))
+        motorLeft.run(powertodegs(basic_speed))
+        motorRight.run(powertodegs(basic_speed))
 
         while abs(motorRight.angle() - start_angle_deg) <= 50:
             if isGreen('r'):
                 isRightGreen = True
-            runleft()
-            runRight()
         motorLeft.hold()
         motorRight.hold()
 
@@ -166,11 +166,19 @@ def onGreenMarker(direction):
             u_turn()
         else:
             print('turn left')
+            tank.drive_for_degrees(30,30,230) # go down 230 deg
+            tank.drive_for_degrees(-30,30,180) # spin turn "left" 180 deg
+            tank.drive(-30,30)
+            while colorLeft.rgb()[1] > highest_refrection_of_Black: # spin turn "left" until left color sensor finds black or green
+                wait(1)
+            tank.drive_for_degrees(-30,30,110) # spin turn "left" 110 deg to be over line
+            motorLeft.hold()
+            motorRight.hold()
     else: # "r" --------------------------------------------------------------------------
         start_angle_deg = motorLeft.angle()
         isLeftGreen = False
 
-        motorLeft.run(powertodegs(basic_speed)) # caching object references
+        motorLeft.run(powertodegs(basic_speed))
         motorRight.run(powertodegs(basic_speed))
 
         while abs(motorLeft.angle() - start_angle_deg) <= 50:
@@ -183,6 +191,14 @@ def onGreenMarker(direction):
             u_turn()
         else:
             print('turn right')
+            tank.drive_for_degrees(30,30,230) # go down 230 deg
+            tank.drive_for_degrees(30,-30,180) # spin turn "right" 180 deg
+            tank.drive(30,-30)
+            while colorRight.rgb()[1] > highest_refrection_of_Black: # spin turn "right" until left color sensor finds black or green
+                wait(1)
+            tank.drive_for_degrees(30,-30,110) # spin turn "right" 110 deg to be over line
+            motorLeft.hold()
+            motorRight.hold()
 
 def isGreen(direction):
     if direction == "l":
@@ -230,6 +246,9 @@ def main():
             # wait(100)
             count += 1
             wait(20)
+        
+        motorLeft.hold()
+        motorRight.hold()
         
         while any(ev3.buttons.pressed()):
             wait(10)
