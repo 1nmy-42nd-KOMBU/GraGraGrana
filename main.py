e138b56d07839676ce0480a888c52d1906a3076a
koki0517 <koki1743ok@gmail.com>
2023-02-10 17:00:43 +0900

    あんま安定しない

--- main.py
+++ main.py
@@ -5,10 +5,16 @@ from pybricks.tools import wait
 
 arduino = UARTDevice(Port.S1, 38400,100)
 arduino.clear()
+wait(100)
+print("start")
 
 for i in range(1000):
     arduino.write((10).to_bytes(1,'big'))
     while arduino.waiting() < 1:
-        wait(1)
+        wait(10)
+        print("error")
+        arduino.write((10).to_bytes(1,'big'))
     whatread = arduino.read()
-    print(whatread[0])
+    if whatread[0] != 10:
+        print(whatread[0])
+    wait(5)
